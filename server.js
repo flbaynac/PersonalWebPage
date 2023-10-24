5fd0f919649bb943eb4f3fc5d4a6756085169f1c
Facundo Baynac <facundobaynac@gmail.com>
2023-10-24 11:47:24 -0300

    move to tagspaces

--- server.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-const PORT  = 9100;
-var connect = require('connect');
-
-connect()
-.use(connect.static(__dirname + '/dist'))
-.listen(PORT);
-
-console.log('Server is running on port: ' + PORT);
